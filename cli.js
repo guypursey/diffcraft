60aadf23745260e3fdd68bc77350361c1bee50ad
Guy Pursey <guypursey@gmail.com>
2020-04-25 17:10:03 +0100

    Added file and reversal options to CLI to allow piping.

--- cli.js
+++ cli.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
 const fsp = require("fs").promises;
+const pipe = require("pipe-args").load();
 const argv = require("yargs")
   .usage("Usage: $0 [options]")
-  .command("Compare two similar files, FILE A and FILE B, and choose the differences from FILE B you'd want to use to edit FILE A.")
+  .command("Craft a patch output based on the differences between two strings/files.")
   .example("$0 -f filea.md fileb.md")
-  .alias("f", "file")
-  .nargs("f", 2)
-  .describe("f", "Compare two files")
-  //.demandOption(["f"])
+  .alias("f", "file").nargs("f", 2)
+  .describe("f", "Craft patch from diff of two files")
+  .nargs("f1", 1)
+  .conflicts("f1", "f")
+  .describe("f1", "Use contents of specified file for first input to diff")
+  .nargs("f2", 1)
+  .conflicts("f2", "f")
+  .describe("f2", "Use contents of specified file for second input to diff")
+  .alias("r", "reverse").nargs("r", 0)
+  .count("r")
+  .describe("r", "Reverse first and second input")
   .help("h")
   .alias("h", "help")
   .alias("v", "version")
@@ -17,28 +25,37 @@ const argv = require("yargs")
 const prompt = require("prompt-sync")()
 const diffCraft = require("./index.js");
 
-if (argv.file) {
-  Promise.all([
-        fsp.readFile(argv.file[0], "utf8"),
-        fsp.readFile(argv.file[1], "utf8")
-      ])
-    .then(function ([filea, fileb]) {
-      return diffCraft.producePatchStringFromFilesContent([
-        {
-          filename: argv.file[0],
-          contents: filea
-        },
-        {
-          filename: argv.file[1],
-          contents: fileb
-        }
-      ], prompt, console.log)
-    })
-    .then(function (data) {
-      console.log("Result")
-      console.log(data)
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
-}
+let r = (argv.r || 0) % 2
+
+Promise.all([
+      argv.f
+        ? fsp.readFile(argv.f[r], "utf8")
+        : argv[`f${r + 1}`]
+          ? fsp.readFile(argv[`f${r + 1}`], "utf8")
+          : `${argv._[r]}${argv._[r].endsWith("\n") ? "" : "\n"}`,
+      argv.f
+        ? fsp.readFile(argv.f[r + 1], "utf8")
+        : argv[`f${r + 2}`]
+          ? fsp.readFile(argv[`f${r + 2}`], "utf8")
+          : `${argv._[r + 1]}${argv._[r + 1].endsWith("\n") ? "" : "\n"}`
+    ])
+  .then(function ([source, edited]) {
+    return diffCraft.producePatchStringFromFilesContent([
+      {
+        "filename": argv.f ? argv.f[r] : argv.f1,
+        "contents": source
+      },
+      {
+        "filename": argv.f ? argv.f[r] : argv.f2,
+        "contents": edited
+      }
+    ]
+    , prompt, console.log)
+  })
+  .then(function (data) {
+    console.log("Result")
+    console.log(data)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
